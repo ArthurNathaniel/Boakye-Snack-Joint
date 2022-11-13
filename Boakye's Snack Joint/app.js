5cb5aa4e7a25c8bb194a4e326ca31453e8b069e1
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2022-11-13 13:28:01 +0000

    Add files via upload

---
+++ Boakye's Snack Joint/app.js
@@ -0,0 +1,6 @@
+document.getElementById("donate").onclick = function () {
+  document.getElementById("title").innerHTML =
+    "Support Boakye Snack Joint <br> Momo Number: 0548811274 <br> Momo name: Boakye <br> Thank you!";
+  document.getElementById("donate").style.display = "none";
+  // alert("Momo number:0548811274" +" " + "Thank You!")
+};
